a936fd9b62a07cbb8a008e5012db20da43f36728
ouzerzou <mohamed.ouz2016@gmail.com>
2024-02-19 19:38:46 +0100

    module

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,23 +5,22 @@ import json
 import sys
 
 
-user_id = sys.argv[1]
-""" the url of the api"""
-api_url = f"https://jsonplaceholder.typicode.com/"
-""" request to get the data"""
-response = requests.get(api_url + f"users?id={user_id}")
-data = response.json()
-user_name = data[0].get("name")
-response = requests.get(api_url + f"todos?userId={user_id}")
-data = response.json()
-allt = len(data)
-tasks_true = 0
-titles = []
-for item in data:
-    if item["completed"]:
-        tasks_true += 1
-        titles.append(item["title"])
-""" print data"""
-print(f"Employee {user_name} is done with tasks({tasks_true}/{allt}):")
-for title in titles:
-    print(f"     {title}")
+if __name__ == "__main__":
+    """ request the data and print it"""
+    user_id = sys.argv[1]
+    api_url = f"https://jsonplaceholder.typicode.com/"
+    response = requests.get(api_url + f"users?id={user_id}")
+    data = response.json()
+    user_name = data[0].get("name")
+    response = requests.get(api_url + f"todos?userId={user_id}")
+    data = response.json()
+    allt = len(data)
+    tasks_true = 0
+    titles = []
+    for item in data:
+        if item["completed"]:
+            tasks_true += 1
+            titles.append(item["title"])
+    print(f"Employee {user_name} is done with tasks({tasks_true}/{allt}):")
+    for title in titles:
+        print(f"     {title}")
