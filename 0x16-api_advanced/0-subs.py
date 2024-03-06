3e3a540a4babe194731abc46dc00f364f5d95b3d
ouzerzou <mohamed.ouz2016@gmail.com>
2024-03-06 14:02:58 +0100

    check 5 and check 6

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,9 +10,6 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, allow_redirects=False, headers=headers)
     if response.status_code == 200:
-        if "data" in response.json() and "subscribers" in response.json()["data"]:
-            return response.json().get('data').get('subscribers')
-        else:
-            return 0
+        return response.json().get('data').get('subscribers')
     else:
         return 0
