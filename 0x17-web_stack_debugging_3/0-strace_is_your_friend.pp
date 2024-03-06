b15ff111aecff27dbc1587147591330fdba694b9
mohamed ouzerzou <mohamed.ouz2016@gmail.com>
2024-03-06 20:54:54 +0000

    new check

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 #Strace is your friend
 
-exec{'fix_error_phpp':
-	command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-	path => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
+exec { 'fix_phpp':
+	command	=> 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+	path	=> ['/bin', '/usr/bin/', '/usr/loca/bin/'],
 }
