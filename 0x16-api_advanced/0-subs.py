fd4f3820ba145a13e7afa738e1b1d8216c23e280
ouzerzou <mohamed.ouz2016@gmail.com>
2024-03-06 13:57:13 +0100

    fix errors

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,6 +10,9 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, allow_redirects=False, headers=headers)
     if response.status_code == 200:
-        return response.json().get('data').get('subscribers')
+        if "data" in response.json() and "subscribers" in response.json()["data"]:
+            return response.json().get('data').get('subscribers')
+        else:
+            return 0
     else:
         return 0
