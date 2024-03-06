9ec080d6414df6be70f0c258dd23d9b507f59f62
mohamed ouzerzou <mohamed.ouz2016@gmail.com>
2024-03-06 21:07:03 +0000

    new test*

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 #Strace is your friend
 
-exec { 'fix_phpp':
-	command	=> 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-	provider => 'shell',
+exec { 'command':
+  command  => 'sed -i \'s/phpp/php/g\' /var/www/html/wp-settings.php',
+  provider => 'shell',
 }
