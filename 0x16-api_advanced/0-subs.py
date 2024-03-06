7682ee57ceba065de9d46bd974af73f6192b6363
ouzerzou <mohamed.ouz2016@gmail.com>
2024-03-06 13:51:45 +0100

    fix errors

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,7 +2,7 @@
 """How many subs?"""
 import requests
 
-headers = {"User-Agent": "MyCustomUserAgent/1.0"}
+headers = {"User-Agent": "AlxUserAgent/1.0"}
 
 
 def number_of_subscribers(subreddit):
@@ -10,7 +10,6 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, allow_redirects=False, headers=headers)
     if response.status_code == 200:
-        data = response.json()
-        return data["data"]["subscribers"]
+        return response.json().get('data').get('subscribers')
     else:
         return 0
