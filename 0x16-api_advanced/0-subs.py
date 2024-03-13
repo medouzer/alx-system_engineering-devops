09dcdd08838e24a69aa54c89c18ec98083cd63da
mohamed ouzerzou <medouz2016@gmail.com>
2024-03-13 15:25:15 -0700

    new things

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,13 +2,14 @@
 """How many subs?"""
 import requests
 
-headers = {"User-Agent": "AlxUserAgent/1.0"}
+headers = {"User-Agent": "user_agent_text"}
 
 
 def number_of_subscribers(subreddit):
     """number of subscribers"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, allow_redirects=False, headers=headers)
+    print(response.status_code)
     if response.status_code == 200:
         return response.json().get('data').get('subscribers')
     else:
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-alx-system_engineering-devops
+0x1A-application_server
